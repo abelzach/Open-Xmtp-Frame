1ac54017dba3cade83f063a748cd785f8b04101f
alanphil2k01 <alanphilipose@gmail.com>
2024-10-31 22:51:12 +0530

    add: game details frame

--- src/app/frames/gameDetails/route.tsx
+++ src/app/frames/gameDetails/route.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/jsx-key */
 import { Button } from "frames.js/next";
-import { frames } from "./frames";
-
+import { frames } from "@/app/frames/frames";
+ 
 const handleRequest = frames(async (ctx) => {
   let iAm: string | undefined;
-
+ 
   if (ctx.message) {
     iAm = (await ctx.message.walletAddress()) ?? "anonymous";
   }
-
+ 
   return {
     image: (
       <div
@@ -38,9 +38,10 @@ const handleRequest = frames(async (ctx) => {
     imageOptions: {
       dynamic: true,
     },
-    buttons: [<Button action="post" target="/gameDetails">Let's go?</Button>],
+    buttons: [
+        <Button action="post">Promote</Button>],
   };
 });
-
+ 
 export const GET = handleRequest;
 export const POST = handleRequest;
